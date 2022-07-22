8c6db25aa4ec5592339995cc07686c7b7e93a6e9
pablo102 <pablo.savkin@gmail.com>
2022-07-22 14:23:00 +0300

    Task 7 finishing

---
+++ Task7/query.sql
@@ -0,0 +1,8 @@
+SELECT
+  pid,
+  now() - pg_stat_activity.query_start AS duration,
+  query,
+  state
+FROM pg_stat_activity
+WHERE (now() - pg_stat_activity.query_start) > interval SECOND(5);
+SELECT pg_cancel_backend(__pid__);
